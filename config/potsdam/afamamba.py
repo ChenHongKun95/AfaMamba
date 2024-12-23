c93f447921891fd669a7b6703dc6eab0dda03941
ChenHongKun95 <1851430749@qq.com>
2024-12-23 16:09:07 +0800

    Update afamamba.py

--- config/potsdam/afamamba.py
+++ config/potsdam/afamamba.py
@@ -2,7 +2,7 @@
 from torch.utils.data import DataLoader
 from geoseg.losses import *
 from geoseg.datasets.potsdam_dataset import *
-from geoseg.DynamicMamba.AfaMamba import AFA
+from geoseg.models.AfaMamba import AFA
 from catalyst.contrib.nn import Lookahead
 from catalyst import utils
 
